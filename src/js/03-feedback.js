79fa577fc4014df6e260ef9a1a33f97a0a4adff5
Kopyl10 <kopyl10@gmail.com>
2025-06-28 11:51:45 -0700

    change 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+if (savedData.email) form.elements.email.value = savedData.email;
+if (savedData.message) form.elements.message.value = savedData.message;
+
+form.addEventListener(
+  "input",
+  throttle(() => {
+    const data = {
+      email: form.elements.email.value.trim(),
+      message: form.elements.message.value.trim(),
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  }, 500)
+);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const submittedData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
+
+  console.log("Submitted:", submittedData);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
